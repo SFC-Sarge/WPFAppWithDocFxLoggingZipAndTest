a2cb0384daf9d1ba00997335986b40cb2c9c9917
Danny McNaught <danny.mcnaught@dannymcnaught.com>
2021-12-07 14:06:35 -0500

    Updated Project Capabilities to include 'Microsoft.Extensions.Logging, DocFx.Console, and System.IO.Compression

--- WPFAppWithDocFxLoggingZipAndTest/source.extension.cs
+++ WPFAppWithDocFxLoggingZipAndTest/source.extension.cs
@@ -9,9 +9,10 @@ namespace WPFAppWithDocFxLoggingZipAndTest
     {
         public const string Id = "WPFAppWithDocFxLoggingZipAndTest.93b6342c-0c8f-4c50-87e6-f0f96cad23b1";
         public const string Name = "WPFAppWithDocFxLoggingZipAndTest";
-        public const string Description = @"Microsoft Test app for WPFAppWithDocFxLoggingZip Template under Visual Studio 2022.";
+        public const string Description = @"Microsoft Test app for WPFAppWithDocFxLoggingZip Template under Visual Studio 2022.
+Updated Project Capabilities to include 'Microsoft.Extensions.Logging, DocFx.Console, and System.IO.Compression";
         public const string Language = "en-US";
-        public const string Version = "15.0";
+        public const string Version = "17.0";
         public const string Author = "Danny McNaught";
         public const string Tags = "WPF; CSharp; Test; Zip; Logging";
     }
